5fd96a7bafc6972c1f121d9f0251da7b5193e551
vovascool <mirdydik@icloud.com>
2023-04-14 18:46:54 +0300

    Добавлены unit тесты для MoviesLoader и ArrayExtension

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,52 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Mir on 14.04.2023.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+class MoviesLoaderTests: XCTestCase {
+    func testSuccessLoading() throws {
+        // Given
+        let stubNetworkClient = StubNetworkClient(emulateError: false)
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
+        // When
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+            // Then
+            switch result {
+            case .success(let movies):
+                XCTAssertEqual(movies.items.count, 2)
+                expectation.fulfill()
+            case .failure(_):
+                XCTFail("Unexpected failure")
+            }
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testFailureLoading() throws {
+        // Given
+        let stubNetworkClient = StubNetworkClient(emulateError: true)
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
+        // When
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+            // Then
+            switch result {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            case.success(_):
+                XCTFail("Unexpected failure")
+            }
+        }
+            waitForExpectations(timeout: 1)
+    }
+}
